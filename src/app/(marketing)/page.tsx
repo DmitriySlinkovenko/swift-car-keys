899359f17cd798b8bacaeab49b40f951825524b2
Dmitriy <dmitriy.slinkovenko@gmail.com>
2024-10-24 21:47:49 -0400

    Clerk setup

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
 
 export default function Home() {
-  return null
+  return (
+    <>
+      <section className="min-h-screen bg-[radial-gradient(hsl(0,72%,65%,40%),hsl(24,62%,73%,40%),hsl(var(--background))_60%)]">
+
+      </section>
+    </>
+  )
 }
